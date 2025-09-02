b53d3c6e6427290e9aa94e27567832e4affc4246
Emelianov Artem <yaz008.yaz008@yandex.ru>
2025-09-02 17:57:34 +0300

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,76 @@
+from datetime import timedelta
+
+from telebot.types import Message
+
+from modules.loader import load_json, load_text
+from modules.quota import TimeQuota
+from project.bot import bot
+
+
+admins: frozenset[int] = load_json('configs/admins.json')
+quotas: dict[str, int] = load_json('configs/quotas.json')
+pending: set[int] = set()
+minutely_quota: TimeQuota[int] = TimeQuota(
+    delta=timedelta(minutes=1),
+    quota=quotas.get('minutely-per-member', 3),
+)
+daily_quota: TimeQuota[int] = TimeQuota(
+    delta=timedelta(days=1),
+    quota=quotas.get('daily-per-member', 10),
+)
+total_quota: TimeQuota[str] = TimeQuota(
+    delta=timedelta(hours=1),
+    quota=quotas.get('hourly-total', 20),
+)
+
+
+@bot.message_handler(commands=['start'])  # type: ignore[misc]
+def on_start(message: Message) -> None:
+    bot.send_message(chat_id=message.chat.id, text=load_text('texts/start.txt'))
+
+
+@bot.message_handler(commands=['new'])  # type: ignore[misc]
+def on_new(message: Message) -> None:
+    pending.add(message.chat.id)
+    bot.send_message(chat_id=message.chat.id, text=load_text('texts/new.txt'))
+
+
+@bot.message_handler(content_types=['text', 'photo'])  # type: ignore[misc]
+def on_report(message: Message) -> None:
+    if message.chat.id not in pending:
+        bot.send_message(
+            chat_id=message.chat.id,
+            text=load_text('texts/errors/not-pending.txt'),
+        )
+    else:
+        match message.chat.id:
+            case member if minutely_quota.exceeds(member):
+                bot.send_message(
+                    chat_id=member,
+                    text=load_text('texts/errors/minutely-quota-exceeded.txt'),
+                )
+            case member if daily_quota.exceeds(member):
+                bot.send_message(
+                    chat_id=member,
+                    text=load_text('texts/errors/daily-quota-exceeded.txt'),
+                )
+            case member if total_quota.exceeds('total'):
+                bot.send_message(
+                    chat_id=member,
+                    text=load_text('texts/errors/total-quota-exceeded.txt'),
+                )
+            case _:
+                for receiver in admins:
+                    bot.forward_message(
+                        chat_id=receiver,
+                        from_chat_id=member,
+                        message_id=message.id,
+                    )
+                bot.send_message(
+                    chat_id=message.chat.id,
+                    text=load_text('texts/success.txt'),
+                )
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
